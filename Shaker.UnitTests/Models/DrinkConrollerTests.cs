5b7f90ebf70ed0ba6c5755f17bcc40e34de47b10
Wojciech Larecki <wojtek.larecki@gmail.com>
2021-09-23 15:38:47 +0200

    add DrinkControllerTests

---
+++ Shaker.UnitTests/Models/DrinkConrollerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using Shaker.WebUI.Controllers;
+using Shaker.WebUI.Models;
+using Shaker.WebUI.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shaker.UnitTests.Models
+{
+    class DrinkConrollerTests
+    {
+        [Test]
+        public void Index_WhenCalled_CheckIfDrinkReposiotryMethodIsCalled()
+        {
+            Mock<IDrinkRepository> dr = new();
+            dr.Setup(dr => dr.GetAllCoctailsByLetter('a'));
+            DrinkController dc = new(dr.Object);
+
+            dc.Index('a');
+
+            dr.Verify(r => r.GetAllCoctailsByLetter('a'));
+        }
+
+        [Test]
+        public void Index_WithoutArgument_ReturnActionResult()
+        {
+            Mock<IDrinkRepository> dr = new();
+            DrinkController dc = new(dr.Object);
+
+            ViewResult actual = dc.Index() as ViewResult;
+
+            Assert.That(actual.Model, Is.Null);
+        }
+    }
+}
---
+++ Shaker.WebUI/Controllers/DrinkController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shaker.WebUI.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shaker.WebUI.Controllers
+{
+    public class DrinkController : Controller
+    {
+        private readonly IDrinkRepository _drinkRepository;
+
+        public DrinkController(IDrinkRepository dr)
+        {
+            _drinkRepository = dr;
+        }
+
+        public IActionResult Index(char? drinkFirstLetter = null)
+        {
+            if (drinkFirstLetter.HasValue)
+            {
+                var drinks = _drinkRepository.GetAllCoctailsByLetter(drinkFirstLetter.Value);
+
+                return View(drinks);
+            }
+            return View();
+        }
+    }
+}
