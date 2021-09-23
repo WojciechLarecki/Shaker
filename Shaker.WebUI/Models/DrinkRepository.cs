8a45802af45493ce038c285fe17b61cdafec0764
Wojciech Larecki <wojtek.larecki@gmail.com>
2021-09-23 12:41:03 +0200

    Create Drink, DrinkRepository, IDrinkRepository class

---
+++ Shaker.WebUI/Models/DrinkRepository.cs
@@ -0,0 +1,16 @@
+﻿using Shaker.WebUI.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shaker.WebUI.Models
+{
+    public class DrinkRepository : IDrinkRepository
+    {
+        public IEnumerable<Drink> GetAllCoctailsByLetter(char a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Shaker.WebUI/Models/Interfaces/IDrinkRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Shaker.WebUI.Models.Interfaces
+{
+    public interface IDrinkRepository
+    {
+        IEnumerable<Drink> GetAllCoctailsByLetter(char a);
+    }
+}
